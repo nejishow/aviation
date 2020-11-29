4436e361a07d96ba301c7f83ad5b32a402b8f7fb
nejishow <ashrafmuhamad2014@gmail.com>
2020-11-29 10:29:56 +0300

    404

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -1,13 +1,26 @@
 <template>
-    <div class="page">
-        
-    </div>
+  <div class="page"></div>
 </template>
+<script>
+export default {
+  metaInfo: {
+    // if no subcomponents specify a metaInfo.title, this title will be used
+    title: "404",
+    meta: [
+      {
+        vmid: "description",
+        name: "description",
+        content: "Page non trouvée",
+      },
+    ],
+  },
+};
+</script>
 <style lang="scss" scoped>
 @import "../sass/main.scss";
-.page{
-    background-image: url("../assets/Terminal-Container-Night-View.jpeg");
-    width: 100%;
-    height: 100vh;
+.page {
+  background-image: url("../assets/Terminal-Container-Night-View.jpeg");
+  width: 100%;
+  height: 100vh;
 }
 </style>
